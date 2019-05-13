b89408ba7244da615abf1fd4b2da4c64967d7e7a
Miska Rantala <31501140+MiskaRantala@users.noreply.github.com>
2019-05-13 18:15:54 +0300

    Add custom login form
    Created a login form with own error messages and texts.

---
+++ spring-security-demo-03-custom-login-form/src/main/java/miskarantala/config/DemoSecurityConfig.java
@@ -0,0 +1,32 @@
+package miskarantala.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        // add our users for in memory authentication
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("Simo").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("Waltteri").password("test123").roles("EMPLOYEE"))
+                .withUser(users.username("Miska").password("test123").roles("MANAGER"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser").permitAll();
+    }
+}
